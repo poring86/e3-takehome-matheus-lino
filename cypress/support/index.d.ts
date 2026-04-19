2bda32b7ba144007ec117f8fac9431b518ca8e5c
poring86 <matheuslino86a@gmail.com>
2026-04-19 16:27:57 -0400

    chore: remove all Cypress files and configuration
    - Deleted all Cypress test files, configs, and support code
    - Removed cypress directory from project root
    - Ready for alternative E2E or manual testing

--- cypress/support/index.d.ts
+++
@@ -1,9 +0,0 @@
-// Tipagem customizada para comandos Cypress
-// https://docs.cypress.io/guides/tooling/typescript-support#Types-for-custom-commands
-
-declare namespace Cypress {
-  interface Chainable {
-    login(email: string, password: string): Chainable<void>;
-    switchOrg(orgName: string): Chainable<void>;
-  }
-}
